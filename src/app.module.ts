4d45c33e3f4911ac5f821fbc8ace8c52b11dcd69
ozudev <ozudev@gmail.com>
2025-03-11 02:49:13 +0300

    typeorm, gerekli dosyalar olusturuldu ve database baglantisi yapildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'secret',
+      database: 'task-management-github',
+      autoLoadEntities: true, // entity isimindeki dosyalari otomatik olarak yukler
+      synchronize: true, // veritabanının semasini otomatik olarak oluşturur
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// main module icinde  forRoot kullanilirken, alt modullerde forFeature kullanilir
